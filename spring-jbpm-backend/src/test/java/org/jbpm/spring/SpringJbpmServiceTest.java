bf9383debb096f9290c719ffc480de92bbcd7d6e
Alejandro Such <alejandro.such@gmail.com>
2016-02-23 14:02:45 +0100

    no message

--- spring-jbpm-backend/src/test/java/org/jbpm/spring/SpringJbpmServiceTest.java
+++ spring-jbpm-backend/src/test/java/org/jbpm/spring/SpringJbpmServiceTest.java
@@ -6,6 +6,8 @@ import static org.junit.Assert.assertNotNull;
 import java.util.Collection;
 import java.util.List;
 
+import org.junit.*;
+
 import org.jbpm.kie.services.impl.KModuleDeploymentUnit;
 import org.jbpm.services.api.DeploymentService;
 import org.jbpm.services.api.ProcessService;
@@ -24,63 +26,64 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@Ignore
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration (locations = {"classpath:/applicationContext.xml"})
 public class SpringJbpmServiceTest extends AbstractTest {
 
 	@Autowired
 	private DeploymentService deploymentService;
-	
+
 	@Autowired
 	private RuntimeDataService runtimeDataService;
-	
+
 	@Autowired
 	private ProcessService processService;
-	
+
 	@Autowired
 	private UserTaskService userTaskService;
-	
+
 	@Test
 	public void testCompleteOperationSet() {
 		assertNotNull(deploymentService);
-		
+
 		Collection<DeployedUnit> units = deploymentService.getDeployedUnits();
 		assertNotNull(units);
 		assertEquals(0, units.size());
-		
-		
+
+
 		DeploymentUnit unit = new KModuleDeploymentUnit(GROUP_ID, ARTIFACT_ID, VERSION);
 		deploymentService.deploy(unit);
-		
+
 		units = deploymentService.getDeployedUnits();
 		assertNotNull(units);
 		assertEquals(1, units.size());
-		
+
 		Collection<ProcessDefinition> processes = runtimeDataService.getProcesses(new QueryContext());
 		assertNotNull(processes);
 		assertEquals(1, processes.size());
-		
+
 		assertNotNull(processService);
 		Long processInstanceId = processService.startProcess(unit.getIdentifier(), "hiring");
 		assertNotNull(processInstanceId);
-		
+
 		ProcessInstanceDesc piDesc = runtimeDataService.getProcessInstanceById(processInstanceId);
 		assertNotNull(piDesc);
 		assertEquals(1, piDesc.getState().intValue());
-		
+
 		assertNotNull(userTaskService);
 		String userId = "john";
 		List<TaskSummary> tasks = runtimeDataService.getTasksAssignedAsPotentialOwner(userId, new QueryFilter());
 		assertNotNull(tasks);
 		assertEquals(1, tasks.size());
-		
+
 		long taskId = tasks.get(0).getId();
 		userTaskService.claim(taskId, userId);
-		
+
 		userTaskService.start(taskId, userId);
-		
+
 		userTaskService.complete(taskId, userId, null);
 	}
-	
-   
+
+
 }
